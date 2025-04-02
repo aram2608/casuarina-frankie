99b14a3f1b671ffbda0c44656eb6d8183632074b
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-04-02 17:18:59 -0400

    FastQC analysis of files

--- fastqc.sh
+++ fastqc.sh
@@ -7,15 +7,13 @@ directory=$1 #stores a directory from a command line argument
 echo "$directory will now be processed" #echos the directory to be processed
 
 #FastQC analysis for the files in the provided directory
-for fastq_file in $(ls $directory) #loops through directory
-do
-if [[ $fastq_file == *fastq.gz ]] #test if files are fastq files, must be zipped files
-    then
-    fastqc $(realpath $directory/$fastq_file); #perfomrs fastQC analysis for all files in directory
-    echo $fastq_file now being processed
+for fastq_file in $directory/*;do #loops through directory
+    echo Starting $fastq_file
+    if [[ $fastq_file == *fastq.gz ]];then #test if files are fastq files, must be zipped files
+        fastqc -o . $fastq_file #perfomrs fastQC analysis for all files in directory
     else
     echo $fastq_file is not a fastq file #prints which files can not be processed
-fi
+    fi
 done
 #lets you now how long the script has run
 date 
