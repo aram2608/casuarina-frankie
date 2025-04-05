d004ee562ba593e78e4469db0be782cfda33ddb8
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-04-05 16:42:44 -0400

    changed to GeneMark2 only since no proteome data avaiable

--- sambamGeneMark.sh
+++ sambamGeneMark.sh
@@ -2,6 +2,9 @@
 
 date #prints start time
 sam_file_directory=$1
+$genome_directory=$2
+
+#creates repeats for use in BRAKER2
 
 #tests for arguments
 if [ -z $sam_file_directory ];then
@@ -10,17 +13,22 @@ if [ -z $sam_file_directory ];then
 fi
 #loops through sam directory to perform conversion and braker2 annotation
 for sam_file in $sam_file_directory/*.sam; do
-    echo Converting SAM to BAM
+    echo Converting SAM to BAM for $sam_file
     base_name=$(basename $sam_file)
     output_bam=$sam_file_directory/${base_name%.sam}.bam #replaces extension
-    sorted_bam=sorted_$output_bam
+    sorted_bam=$sam_file_directory/sorted_${base_name%.sam}.bam #creates output file name for sorted BAMs
  
     #samtool conversion
     samtools view -@ 20 -Sb $output_bam $sam_file | samtools sort -O bam -o $sorted_bam $output_bam
-    rm $sam_file
-    echo "$sam_file processed and replaced with sorted BAM"
 
-    if [ -? -ne 0 ]; then #checks for command success and logs failures
+    if [ $PIPESTATUS[0] -ne 0 ] || [ $PIPESTATUS[1] -ne 0 ]; then #checks for command success and logs failures
         echo "Warning: Failed to convert SAM file" >> failurelog.txt
+        echo "Pipe Failure: $PIPESTATUS[@]" >> failurelog.txt
+        continue
     fi
+
+    rm $sam_file
+    echo $sam_file processed and replaced with sorted BAM
+
+
 done
