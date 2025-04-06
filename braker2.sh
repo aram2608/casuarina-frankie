a0fd094c626daecea400b5642e92c99a361918c4
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-04-05 23:08:53 -0400

    braker2 script improvements

--- braker2.sh
+++ braker2.sh
@@ -9,18 +9,28 @@ output_directory=$4 #output directory
 
 #check to ensure all args met
 if [ -z $bam_directory ] || [ -z $genome ] || [ -z $gene_mark_key_dir ] || [ -z $output_directory ]; then
-    echo "Usage: ./braker2.sh <bam_directory> <gene_mark_key_dir>"
+    echo "Usage: ./braker2.sh <bam_directory> <genome> <gene_mark_key_dir> <output_directory>"
     exit 1
 fi
 
+export GENEMARK_PATH=$gene_mark_key_dir
+export GM_KEY="$gene_mark_key_dir/gm_key"
+
 #look through bam files
 for bam_file in $bam_directory/*.bam; do 
-    braker.pl --species=casuarina_glauca \
-    --genome=$genome \
-    --bam=$bam_file \
-    --softmasking \
-    --workingdir=$output_directory
-    echo Finished annotating $bam_file
+    sample_name=$(basename $bam_file .bam)
+    sample_output_dir=$output_directory/$sample_name
+
+    echo Running BRAKER2 for $sample_name
+
+    braker.pl \
+        --species=casuarina_glauca \
+        --genome=$genome \
+        --bam=$bam_file \
+        --softmasking \
+        --workingdir=$output_directory
+
+    echo Finished annotating $sample_name
 done
 
 date #print the end time of script
