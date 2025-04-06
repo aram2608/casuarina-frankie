39837ab4a9e6a68744d8f50e32fd650ed98fc5b4
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-04-05 23:02:20 -0400

    braker2 script structure

--- braker2.sh
+++ braker2.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+date #print start time
+
+bam_directory=$1 #directory for bam files
+genome=$2 #path to genome
+gene_mark_key_dir=$3 #where the gene_mark key is
+output_directory=$4 #output directory
+
+#check to ensure all args met
+if [ -z $bam_directory ] || [ -z $genome ] || [ -z $gene_mark_key_dir ] || [ -z $output_directory ]; then
+    echo "Usage: ./braker2.sh <bam_directory> <gene_mark_key_dir>"
+    exit 1
+fi
+
+#look through bam files
+for bam_file in $bam_directory/*.bam; do 
+    braker.pl --species=casuarina_glauca \
+    --genome=$genome \
+    --bam=$bam_file \
+    --softmasking \
+    --workingdir=$output_directory
+    echo Finished annotating $bam_file
+done
+
+date #print the end time of script
