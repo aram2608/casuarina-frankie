4c1834cc195be9944587af0b1ebe054c3d42cc70
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-04-05 21:09:05 -0400

    hopefully works

--- sambam.sh
+++ sambam.sh
@@ -13,8 +13,8 @@ fi
 for sam_file in $sam_file_directory/*.sam.gz; do
     echo Converting SAM to BAM for $sam_file
 
-    base_name=$(basename $sam_file)
-    sorted_bam=$sam_file_directory/sorted_${base_name%.sam}.bam #creates output file name for sorted BAMs
+    base_name=$(basename $sam_file .sam.gz) #allows removal of suffixes
+    sorted_bam=$sam_file_directory/sorted_${base_name}.bam #creates output file name for sorted BAMs
  
     #samtool conversion and failure checking
     if gunzip -c $sam_file | samtools view -@ 20 -Sb | samtools sort -O bam -o $sorted_bam; then
