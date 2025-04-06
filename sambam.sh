3c29b8030365b258f9beb2d427c09134e6e0b3fd
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-04-05 20:37:10 -0400

    separating sam conversion and braker2 script as there were bugs galore

--- sambam.sh
+++ sambam.sh
@@ -2,9 +2,9 @@
 
 date #prints start time
 sam_file_directory=$1
-$genome_directory=$2
-$masked_repeats_directory=$3
-$species='casuarina_glauca'
+genome_directory=$2
+masked_repeats_directory=$3
+species='casuarina_glauca'
 
 #creates repeats for use in BRAKER2
 
@@ -13,28 +13,23 @@ if [ -z $sam_file_directory ] || [ -z $genome_directory ] || [ -z $masked_repeat
     echo "Usage: ./sambambraker2 <sam_file_directory> <masked_repeats_directory>"
     exit 1 #safely exists program
 fi
+
 #loops through sam directory to perform conversion and braker2 annotation
 for sam_file in $sam_file_directory/*.sam; do
     echo Converting SAM to BAM for $sam_file
+
     base_name=$(basename $sam_file)
     output_bam=$sam_file_directory/${base_name%.sam}.bam #replaces extension
     sorted_bam=$sam_file_directory/sorted_${base_name%.sam}.bam #creates output file name for sorted BAMs
  
     #samtool conversion
-    gunzip $sam_file | samtools view -@ 20 -Sb $output_bam $sam_file | samtools sort -O bam -o $sorted_bam $output_bam
+    gunzip -c $sam_file | samtools view -@ 20 -Sb $output_bam $sam_file | samtools sort -O bam -o $sorted_bam $output_bam
 
     if [ $PIPESTATUS[0] -ne 0 ] || [ $PIPESTATUS[1] -ne 0 ] || [ $PIPESTATUS[3] ]; then #checks for command success and logs failures
         echo "Warning: Failed to convert SAM file" >> failurelog.txt
-        echo "Pipe Failure: $PIPESTATUS[@]" >> failurelog.txt
         continue
     fi
 
     rm $sam_file
     echo $sam_file processed and replaced with sorted BAM
-
-    echo beginning BRAKER2 analysis for gene structure annotation
-
-    #braker2 analysis
-    for genome in $genome_directory\*.fna;do
-    braker.pl --genome $genome --bam $output_bam --softmasking --cores 25 --species $species 
 done
