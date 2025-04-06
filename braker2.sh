d7e717a54f9a8278d81be102f9bbd01ce85d346c
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-04-05 23:40:47 -0400

    braker2 handles weird ls behavior better

--- braker2.sh
+++ braker2.sh
@@ -6,20 +6,27 @@ bam_directory=$1 #directory for bam files
 genome=$2 #path to genome
 output_directory=$3 #output directory
 
-#create a list of bam files to train all at once
-bam_list=$(ls "$bam_directory"/*.bam | paste -sd, -)
-
-#check to see if all bams included
-echo BAM list: $bam_list
-
 #check to ensure all args met
 if [ -z $bam_directory ] || [ -z $genome ] || [ -z $output_directory ]; then
     echo "Usage: ./braker2.sh <bam_directory> <genome> <output_directory>"
     exit 1
 fi
 
+#create a list of bam files separated by commas
+bam_list=$(ls "$bam_directory"/*.bam 2>/dev/null | paste -sd, -) #redirects file descriptor 2 to the unix trash can /dev/null
+
+#check for empty bam list
+if [ -z $bam_list ]; then
+    echo No BAM files found in $bam_directory
+    exit 1
+fi
+
+#check to see if all bams included
+echo BAM list: $bam_list
+
+#set genemark variable
 export GENEMARK_PATH="/home/users/ja1473/gmes_linux_64_4"
-export GM_KEY="/home/users/ja1473/gmes_linux_64_4/gm_key"
+export GM_KEY="$GENEMARK_PATH/gm_key"
 
 #test to ensure genemark info is set
 if [ ! -f $GM_KEY ]; then
@@ -27,7 +34,7 @@ if [ ! -f $GM_KEY ]; then
     exit 1
 fi
 
-#running braker2
+#run braker2
 braker.pl \
     --species=casuarina_glauca \
     --genome=$genome \
