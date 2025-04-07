8a222b6abded315883dcbb31cb7595776b9cf52c
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-04-07 12:13:49 -0400

    trinity script cleanups

--- trinity.sh
+++ trinity.sh
@@ -13,14 +13,12 @@ fi
 
 #make output just in case
 mkdir -p "$trinity_output"
-sample=$(basenam $merged_sorted_bam .bam)
 
 #trinity params
 trinity \
-    --seqType fq \
     --max_memory 50g \
     --genome_guided_bam "$merged_sorted_bam" \
-    --output "$trinity_output/${sample}.fasta" \
+    --output "$trinity_output" \
     --SS_lib_type R \
     --CPU 26 \
     --genome_guided_max_intron 10000 
