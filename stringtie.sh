bf1fda7cbd735d46c55c49f19a5a2a17ff542ddd
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-04-06 21:04:32 -0400

    stringtie improvements, should be good to go

--- stringtie.sh
+++ stringtie.sh
@@ -4,23 +4,30 @@ date #prints start time
 bam_dir=$1 #input directory for bams
 output_dir=$2 #output directory for stringtie
 
-#makes directory just in case
-mkdir -p $output_dir
-
 #a check to ensure args are met
 if [ -z $bam_dir ] || [ -z $output_dir ]; then
     echo "Usage: ./stringtie.sh <bam_dir> <output_dir>"
     exit 1
 fi
 
+#makes directory just in case
+mkdir -p $output_dir
+
 #loop for bam files
 for bam in $bam_dir/*.bam; do
-    $sample=$(basename $bam .bam)
+    sample=$(basename $bam .bam)
     echo Processing $sample
 
     #stringtie params
     stringtie \
         -p 20 \
         -o $output_dir/${sample}.gtf \
-        -l ${$sample} 
+        -l $sample
+
+    #safety check to ensure command ran properly
+    if [ $? -ne 0 ]; then
+        echo Failure in processing $sample >> $output_dir/failurelog.txt
+    fi
 done
+
+date #print end time of script
