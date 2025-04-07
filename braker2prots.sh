c9eb169284b56fe243077ef32cabdbaf5b79ada7
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-04-07 19:33:58 -0400

    fixes

--- braker2prots.sh
+++ braker2prots.sh
@@ -2,27 +2,20 @@
 
 date  #script start time
 
-merged_bam="$1"         #full path to merged BAM file
-$protein_fasta="$2"     #path to protein FASTA
+protein_fasta="$1"      #full path to merged BAM file
+genome="$2"             #path to protein FASTA
 output_directory="$3"   #output directory
 
 #argument check
-if [ -z "$merged_bam" ] || [ -z "$genome" ] || [ -z "$output_directory" ]; then
-    echo "Usage: ./braker3.sh <merged_bam> <genome> <output_directory>"
+if [ -z "$protein_fasta" ] || [ -z "$genome" ] || [ -z "$output_directory" ]; then
+    echo "Usage: ./braker3.sh <protein_fasta> <genome> <output_directory>"
     exit 1
 fi
 
 #bam file validation
-if [ ! -f "$merged_bam" ]; then
-    echo "Merged BAM file not found: $merged_bam"
+if [ ! -f "$protein_fasta" ]; then
+    echo "Merged BAM file not found: $protein_fasta"
     exit 1
-fi
-
-#bam index validation
-if [ ! -f "${merged_bam}.bai" ]; then
-    echo "BAM index not found: ${merged_bam}.bai"
-    echo "Generating index..."
-    samtools index "$merged_bam"
 fi
 
 #run BRAKER2
@@ -39,6 +32,4 @@ braker.pl \
 echo "Finished annotating Casuarina glauca"
 date  #time to finish script
 
-#added strandness flag
 #added gff3 for evm
-#--stranded=- for reverse
