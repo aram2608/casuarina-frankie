73ac7cafbedc0b1476bbc7570f398a8c1866f757
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-04-07 12:10:59 -0400

    trinity script improvements hopefully

--- trinity.sh
+++ trinity.sh
@@ -6,20 +6,21 @@ merged_sorted_bam=$1 #input dir
 trinity_output=$2 #output dir
 
 #check for args and bam files
-if [ ! -f $merged_sorted_bam ] || [ -z $trinity_output ]; then
+if [ ! -f "$merged_sorted_bam" ] || [ -z "$trinity_output" ]; then
     echo "Usage: ./trinity.sh <bam_dir> <trinity_output>"
     exit 1
 fi
 
 #make output just in case
-mkdir -p $trinity_output
+mkdir -p "$trinity_output"
+sample=$(basenam $merged_sorted_bam .bam)
 
 #trinity params
 trinity \
     --seqType fq \
     --max_memory 50g \
-    --genome_guided_bam $merged_sorted_bam \
-    --output $trinity_output \
+    --genome_guided_bam "$merged_sorted_bam" \
+    --output "$trinity_output/${sample}.fasta" \
     --SS_lib_type R \
     --CPU 26 \
     --genome_guided_max_intron 10000 
