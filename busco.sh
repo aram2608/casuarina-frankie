2fca57b756784bdaae8619b9a8ff920ce3d5f839
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-04-11 14:14:59 -0400

    cleaner busco script

--- busco.sh
+++ busco.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-input_GTF_protein_dir=$1 # input file to GTF file ---- /full/path/to/file
-output_dir_1=$(basename "$input_GTF_protein_dir" .gtf)_busco_fabales # output dir
-output_dir_2=$(basename "$input_GTF_protein_dir" .gtf)_busco_eudicots # output dir
+protein_dir=$1 # input to directory
 
 # safety check to make sure args are met
-if [ -z "$input_GTF_protein_dir" ]; then
-    echo "Usage: <input_GTF>"
+if [ -z "$protein_dir" ]; then
+    echo "Usage: <protein_directory>"
     exit
 fi
 
-for prot_GTF in $input_GTF_protein_dir/*.faa; do
+# lopping through protein directory
+for prot_GTF in "$protein_dir"/*.faa; do
+    #creating output directories
+    base_name=$(basename "$prot_GTF" .faa)
+
+    output_dir_1="${base_name}_busco_fabales"
+    output_dir_2="${base_name}_busco_eudicots"
     # busco run parameters
     busco -i "$prot_GTF" \
         -l fabales_odb10 \
@@ -22,7 +26,7 @@ for prot_GTF in $input_GTF_protein_dir/*.faa; do
         -o "$output_dir_2" \
         -m protein
 
-echo "Finished checking quality of $prot_GTF"
+    echo "Finished checking quality of $prot_GTF"
 
 # important params to know for working with casuarina
 # -l fabales_odb10 # most biologically relevant, legumes like medicago
