02131d9492fcb431decd6e64a24c038a1ec3fa2d
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-04-17 11:11:06 -0400

    improvements and bug fixes

--- blast.sh
+++ blast.sh
@@ -4,21 +4,25 @@ sequences=$1 # full path to protein seqs
 output=$2 # full path to output dir
 
 # safety feature for arguments
-if [ -f $sequences ] || [ -z $output ]; then
+if [ ! -f "$sequences" ] || [ -z "$output" ]; then
     echo "Usage: <path_to_proteins> <output_dir_path>"
     exit 1
 fi
 
-echo "Beginning blast search for $sequences"
+# make output just in case
+mkdir -p "$output"
+
+echo "Beginning blast search for '$sequences'..."
+
 # blastp params
 blastp -query $sequences \
     -db nr \
     -outfmt '6 qseqid sseqid qlen slen qstart qend sstart send length pident positive mismatch gaps evalue bitscore qcovs' \
-    -num_alignments 10 
-    | > blastp_10hits.out
+    -num_alignments 10 \
+    -out "$output/blastp_10hits.out"
 
-echo -e 'qseqid\tsseqid\tqlen\tslen\tqstart\tqend\tsstart\tsend\tlength\tpident\tpositive\tmismatch\tgaps\tevalue\tbitscore\tqcovs' > header1.tab
-cat header1.tab blastp_10hits.out > blastp_10hits.tsv
+echo -e 'qseqid\tsseqid\tqlen\tslen\tqstart\tqend\tsstart\tsend\tlength\tpident\tpositive\tmismatch\tgaps\tevalue\tbitscore\tqcovs' > "$output/header1.tab"
+cat "$output/header1.tab" "$output/blastp_10hits.out" > "$output/blastp_10hits.tsv"
 
 echo "Finished processesing $sequences: new file = blastp_10hits.tsv"
 
