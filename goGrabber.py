2918780a01d19ecaae2473c2ca1b3b771c3fe362
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-04-20 15:23:46 -0400

    updated git ignore and better script notes for future self

--- goGrabber.py
+++ goGrabber.py
@@ -1,8 +1,20 @@
 import pandas as pd
 import argparse
 
-# a function to fetch GO terms from an interproscan run
 def main(input_tsv, output_file):
+
+    """
+    Extracts gene IDs and GO terms from an InterProScan TSV file and writes to a new TSV file.
+    To prepare the InterProScan file for extraction, use the following command on your output...
+
+    grep "GO" example_interproscan.tsv > my_new_file.tsv
+
+    This will ensure you have only pulled genes/proteins that are guaranteed to have GO terms.
+    
+    Next ensure that the GO terms are in the 14th column, in pandas this is represented as index[13]
+
+    Example Usage: python3 goGrabber.py /path/to/my_new_file.tsv /path/to/workdir/goterms.tsv
+    """
 
     # load TSV as a dataframe
     df = pd.read_csv(input_tsv, sep="\t", header=None)
@@ -15,10 +27,10 @@ def main(input_tsv, output_file):
     new_data_frame.insert(1, "go_terms", df[13])
     
     # writes to new csv file
-    new_data_frame.to_csv(output_file, sep="\t")
+    new_data_frame.to_csv(output_file, sep="\t", index=False)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Remove isoforms from an eggnogmapper annotation file.")
+    parser = argparse.ArgumentParser(description="Extract Gene IDs and GO terms from InterProScan tsv.")
     parser.add_argument("input_tsv", help="Input annotation file (TSV format)")
     parser.add_argument("output_file", help="Name of output file")
     args = parser.parse_args()
